5b81a40fc7a4fc6c59dcd5c3a653243baf6a6a3e
Lyp <l1yp@qq.com>
2022-10-26 08:45:55 +0800

    context-pad扩展验证完成

--- src/components/bpmn/context-pad/ContextPad.ts
+++ src/components/bpmn/context-pad/ContextPad.ts
@@ -1,18 +1,70 @@
+import { assign } from "min-dash";
+
 export default class CustomContextPad {
-  constructor(config, contextPad, create, elementFactory, injector, translate) {
+
+  static $inject: string[]
+
+  create: any
+  elementFactory: any
+  translate: any
+  autoPlace: any
+  modeling: any
+
+  constructor(config, contextPad, create, elementFactory, injector, translate, modeling) {
     this.create = create
     this.elementFactory = elementFactory
     this.translate = translate
     if (config.autoPlace !== false) {
       this.autoPlace = injector.get('autoPlace', false)
     }
+    this.modeling = modeling
     contextPad.registerProvider(this)
     console.log('init pad');
   }
 
 
-  getContextPadEntrier(element) {
+
+  public getContextPadEntries(element) {
     console.log('getContextPadEntrier', element);
+    const type = element.type
+    if (type === 'bpmn:StartEvent') {
+
+      const create = this.create
+      const elementFactory = this.elementFactory
+      const autoPlace = this.autoPlace
+
+      function appendStart(event, element) {
+
+        const shape = elementFactory.createShape({ type: 'bpmn:UserTask' });
+        create.start(event, shape, {
+          source: element
+        });
+      }
+  
+
+
+      var append = autoPlace ? function(event, element) {
+        var shape = elementFactory.createShape({ type: 'bpmn:UserTask' });
+
+        autoPlace.append(element, shape);
+      } : appendStart;
+      return {
+        'append.append-user-task': {
+          group: 'model',
+          title: 'Append UserTask',
+          className: 'bpmn-icon-user-task',
+          action: {
+            click: append,
+            dragstart: appendStart,
+          }
+        },
+        // 'append.end-event': undefined,
+        // 'append.gateway': undefined,
+        // 'append.intermediate-event': undefined,
+        // 'append.text-annotation': undefined,
+        // replace: undefined,
+      }
+    }
   }
 
 }
@@ -23,5 +75,6 @@ CustomContextPad.$inject = [
   'create',
   'elementFactory',
   'injector',
-  'translate'
+  'translate',
+  'modeling',
 ]
