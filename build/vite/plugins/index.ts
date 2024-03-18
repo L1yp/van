0625b049711d66b5adca38b492671c15f114dd4f
Lyp <l1yp@qq.com>
2024-03-18 15:06:57 +0800

    feat: 新增svg-loader插件

--- build/vite/plugins/index.ts
+++ build/vite/plugins/index.ts
@@ -1,10 +1,12 @@
 import {configSvgIconsPlugin} from "./svg-icon-vite-plugin"
 import vue from '@vitejs/plugin-vue';
 import buildCompression from 'vite-plugin-compression'
+import svgLoader from 'vite-svg-loader'
 
 export function createVitePlugins(viteEnv: Record<string, string>, isBuild: boolean) {
   const plugins = [
     vue(),
+    svgLoader(),
   ];
   plugins.push(configSvgIconsPlugin(isBuild));
   const gzipPlugin = buildCompression({
